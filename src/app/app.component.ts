92be9f80a29fff2a8aa078f5dc757269aea0b3b1
olivero-j <113719936+olivero-j@users.noreply.github.com>
2022-10-15 18:25:24 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  //title: string (si quiero) = 'Contador App';
+  
 }
